ac3865bf171e4f4b0878826a0ab3d0ffa3fb5806
Roberto <roberto@digitalizandoideas.com>
2014-02-11 14:59:16 +0100

    Actualizado index.php

--- index.php
+++ index.php
@@ -8,9 +8,3 @@ defined('ROOT')     ? null : define('ROOT', realpath(dirname(__FILE__)) . DS);
 defined('APP_PATH') ? null : define('APP_PATH', ROOT . 'aplicacion' . DS);
 
 require_once APP_PATH . 'Configs.php';
-require_once APP_PATH . 'Peticion.php';
-require_once APP_PATH . 'Lanzador.php';
-require_once APP_PATH . 'Controlador.php';
-require_once APP_PATH . 'Modelo.php';
-require_once APP_PATH . 'Vista.php';
-require_once APP_PATH . 'Registro.php';
