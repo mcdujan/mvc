f304002a9677950a3ba1ec9a112ccd15ec7d9f3a
Marta <alu20465950k@ieselcaminas.org>
2014-02-12 15:08:09 +0100

    Modificado userModel por error en la subida

--- models/userModel.php
+++ models/userModel.php
@@ -11,16 +11,16 @@ class userModel extends Model {
     }
 
     public function getUserById($id) {
-<<<<<<< HEAD
+
         // Preparar el statement usando bien PDO
-        $user = $this->_db->query("SELECT * FROM usuarios WHERE id=$id");
-        $user = array_shift($user->fetchall());
-        return $user;
-=======
+        //$user = $this->_db->query("SELECT * FROM usuarios WHERE id=$id");
+        //$user = array_shift($user->fetchall());
+        //return $user;
+
         $user = $this->_db->prepare("SELECT * FROM usuarios WHERE id=?");
         $user->bindParam(1, $id, PDO::PARAM_INT);
         $user->execute();
         return array_shift($user->fetchall());
->>>>>>> 32ea74a80e14b59d66e108150eef78a878259ba8
+
     }
 }
