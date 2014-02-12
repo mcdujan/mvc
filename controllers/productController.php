019b1ae3b19eba7150881e59106a065608a780f6
mcdujan <roberto@digitalizandoideas.com>
2014-02-12 21:12:40 +0100

    Editado product controller, model y vista para empezar a codificar

--- controllers/productController.php
+++ controllers/productController.php
@@ -8,4 +8,10 @@ class productController extends Controller {
         parent::__construct();
         $this->_product = $this->loadModel('product');
     }
+
+    public function index(){
+        $this->_view->product = $this->_product->getProducts();
+        $this->_view->title   = "Lista de productos";
+        $this->_view->render('index');
+    }
 }
--- models/productModel.php
+++ models/productModel.php
@@ -1,7 +1,12 @@
 <?php
 
-class userModel extends Model {
+class productModel extends Model {
     public function __construct(){
         parent::__construct();
+    }
+
+    public function getProducts(){
+        $user = $this->_db->query('SELECT * FROM productos');
+        return $user->fetchall();
     }
 }
