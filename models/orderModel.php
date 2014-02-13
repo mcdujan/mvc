c258fb303a2878ee817c8a2ea8508f2c0d1bc681
mcdujan <roberto@digitalizandoideas.com>
2014-02-13 11:11:24 +0100

    Modificaciones order

--- models/orderModel.php
+++ models/orderModel.php
@@ -12,11 +12,12 @@ class orderModel extends Model {
 
     public function getOrderById($id) {
         $order = $this->_db->prepare(
-               "SELECT pedidos.referencia, productos.nombre, pedidos_productos.cantidad, (pedidos_productos.cantidad * productos.precio) AS precio
+               "SELECT pedidos.referencia, productos.nombre, productos.precio, 
+               pedidos_productos.cantidad, 
+               (pedidos_productos.cantidad * productos.precio) AS precio_total
                 FROM pedidos, pedidos_productos, productos
                 WHERE pedidos_productos.id_pedido = ?
                 AND pedidos_productos.id_producto = productos.id"
-               // "SELECT * FROM pedidos WHERE id = ?"                    
             );
         $order->bindParam(1, $id, PDO::PARAM_INT);
         $order->execute();
