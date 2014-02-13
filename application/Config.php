67ae41131ecccac1a2929f41035a6819a967b874
mcdujan <roberto@digitalizandoideas.com>
2014-02-13 09:00:42 +0100

    Actualizado config para evitar conflictos

--- application/Config.php
+++ application/Config.php
@@ -5,7 +5,7 @@
  */
 defined('DB_HOST')              ? null : define('DB_HOST', 'localhost');
 defined('DB_USER')              ? null : define('DB_USER', 'root');
-defined('DB_PASS')              ? null : define('DB_PASS', '');
+defined('DB_PASS')              ? null : define('DB_PASS', 'e4e5e6e7');
 defined('DB_NAME')              ? null : define('DB_NAME', 'mvc');
 defined('DB_CHAR')              ? null : define('DB_CHAR', 'utf8');
 
@@ -14,10 +14,10 @@ defined('DB_CHAR')              ? null : define('DB_CHAR', 'utf8');
  * CONSTANTES DEL PROYECTO
  */  
 
-defined ('BASE_URL')            ? null : define('BASE_URL', 'http://localhost:8088/ejercicios/GitHub/mvc/');
+// defined ('BASE_URL')            ? null : define('BASE_URL', 'http://localhost:8088/ejercicios/GitHub/mvc/');
 
 //Como tenemos definido el localhost diferente lo dejo oculto
-//defined ('BASE_URL')            ? null : define('BASE_URL', 'http://localhost/framework/');
+defined ('BASE_URL')            ? null : define('BASE_URL', 'http://localhost/framework/');
 
 
 // Definición de elementos por defecto
